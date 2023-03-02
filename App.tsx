1de854bd3f0322d3bef608e271e9770f6b64615c
Michael Vitello <michael.vitello@gmail.com>
2023-03-02 22:20:09 +0100

    chore(setup): lint app tsx with added linter options

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import Navigation from './src/navigation';
-import { useFonts } from "expo-font";
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { fonts } from './src/theme/fonts';
-import { ActivityIndicator } from 'react-native';
+import { useFonts } from 'expo-font'
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { ActivityIndicator } from 'react-native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import Navigation from './src/navigation'
+import { fonts } from './src/theme/fonts'
 
 export default function App() {
     const [fontsLoaded] = useFonts({
-      [fonts.regular]: "https://fonts.cdnfonts.com/s/15011/CircularStd-Medium.woff",
-      [fonts.bold]: "https://fonts.cdnfonts.com/s/15011/CircularStd-Bold.woff",
-    });
-    if(!fontsLoaded) return <ActivityIndicator size="large" style={{justifyContent: 'center', flex: 1}} />
+        [fonts.regular]: 'https://fonts.cdnfonts.com/s/15011/CircularStd-Medium.woff',
+        [fonts.bold]: 'https://fonts.cdnfonts.com/s/15011/CircularStd-Bold.woff',
+    })
+
+    if (!fontsLoaded)
+        return <ActivityIndicator size='large' style={{ justifyContent: 'center', flex: 1 }} />
+
     return (
-      <SafeAreaProvider>
-        <Navigation />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
+        <SafeAreaProvider>
+            <Navigation />
+            <StatusBar />
+        </SafeAreaProvider>
+    )
 }
